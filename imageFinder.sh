bbe4271632ce4e2d9a291774d6890cefc1fde24f
professorwrestler <piercem047@gmail.com>
2025-03-24 22:53:28 -0400

    fixed some output text

--- imageFinder.sh
+++ imageFinder.sh
@@ -5,10 +5,10 @@ mkdir newDirectory
 for dir in */; do
   echo "Processing directory: $dir"
   if find ~/Pictures/SAWARATSUKI.ServiceLogos2 -type f -name "*.png" | grep -q "."; then
-    echo "Folder contains PNG files"
+    echo "Folder contains PNG files."
     ls $dir
     cp $dir/*.png newDirectory
   else
-    echo "Folder does not contain PNG files"
+    echo "Folder does not contain PNG files."
   fi
 done
